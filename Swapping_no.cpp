9b6b050213eae56e48ffc3018f901fcbeec6f7f4
Rohan Shinde <120273586+rohanshinde81@users.noreply.github.com>
2024-02-15 23:21:36 +0530

    Create Swapping_no.cpp

---
+++ Swapping_no.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"Enter First values :";
+    cin>>x;
+    cout<<"Enter Second values :";
+    cin>>y;
+    int temp = x;
+         x = y;
+         y = temp;
+ {
+   cout<<"After swapping x is "<<x<<endl;
+   cout<<"After swapping y is "<<y;
+        
+     }
+         
+}
